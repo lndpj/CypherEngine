4477c5348d14cc4d726c816da222ae58e0602ec8
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-12 12:20:28 +0200

    Rename error type in CypherHost_Error.h

--- src/CypherEngine/CypherHost/CypherHost_Error.h
+++ src/CypherEngine/CypherHost/CypherHost_Error.h
@@ -10,7 +10,7 @@ namespace cypher::engine::host
 Host Error Codes
 ================
 */
-enum class error_code_t : common::u8 {
+enum class host_error_t : common::u8 {
     OK = 0,
 
     ERR_NOT_INIT,
@@ -23,37 +23,37 @@ enum class error_code_t : common::u8 {
 Host Error Helpers
 ================
 */
-constexpr inline const char *CypherHost_ErrorName( const error_code_t error ) {
+constexpr inline const char *CypherHost_ErrorName( const host_error_t error ) {
     switch ( error ) {
-    case error_code_t::OK:
+    case host_error_t::OK:
         return "OK";
-    case error_code_t::ERR_NOT_INIT:
+    case host_error_t::ERR_NOT_INIT:
         return "ERR_NOT_INIT";
-    case error_code_t::ERR_IS_INIT:
+    case host_error_t::ERR_IS_INIT:
         return "ERR_IS_INIT";
-    case error_code_t::ERR_INITIALIZING:
+    case host_error_t::ERR_INITIALIZING:
         return "ERR_INITIALIZING";
     default:
         return "ERR_UNKNOWN";
     }
 }
 
-constexpr inline const char *CypherHost_ErrorDesc( const error_code_t error ) {
+constexpr inline const char *CypherHost_ErrorDesc( const host_error_t error ) {
     switch ( error ) {
-    case error_code_t::OK:
+    case host_error_t::OK:
         return "operation completed successfully";
-    case error_code_t::ERR_NOT_INIT:
+    case host_error_t::ERR_NOT_INIT:
         return "host subsystem is not initialized";
-    case error_code_t::ERR_IS_INIT:
+    case host_error_t::ERR_IS_INIT:
         return "host subsystem is already initialized";
-    case error_code_t::ERR_INITIALIZING:
+    case host_error_t::ERR_INITIALIZING:
         return "host subsystem failed while initializing";
     default:
         return "unknown host error";
     }
 }
 
-constexpr inline common::error_t CypherHost_ErrorCode( error_code_t error ) {
+constexpr inline common::error_t CypherHost_ErrorCode( host_error_t error ) {
     return common::CypherCommon_ErrorMake( common::domain_t::COM_DOMAIN_HOST, static_cast<common::com_u16>( error ) );
 }
 
