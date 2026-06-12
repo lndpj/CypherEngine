9e2d977ca4426fe11c36557f8c5f9460a57a05c8
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-12 17:35:17 +0200

    Update filesystem runtime helpers

--- src/CypherEngine/CypherFileSystem/CypherFileSystem_Runtime.cpp
+++ src/CypherEngine/CypherFileSystem/CypherFileSystem_Runtime.cpp
@@ -6,6 +6,7 @@ namespace cypher::engine::fs
 namespace {
 
 runtime_state_t g_fs_runtime_state{};
+std::recursive_mutex g_fs_runtime_mutex{};
 
 }       // namespace
 
@@ -14,14 +15,21 @@ runtime_state_t &CypherFileSystem_RuntimeState()
     return g_fs_runtime_state;
 }
 
+std::recursive_mutex &CypherFileSystem_RuntimeMutex()
+{
+    return g_fs_runtime_mutex;
+}
+
 bool CypherFileSystem_HasWritePath()
 {
+    std::lock_guard<std::recursive_mutex> lock( CypherFileSystem_RuntimeMutex() );
     const runtime_state_t &state = CypherFileSystem_RuntimeState();
     return state.initialized && state.write_path[0] != '\0';
 }
 
 fs_error_t CypherFileSystem_BuildWritePath( const char *virtual_path, char *out_path, common::u32 out_path_size )
 {
+    std::lock_guard<std::recursive_mutex> lock( CypherFileSystem_RuntimeMutex() );
     runtime_state_t &state = CypherFileSystem_RuntimeState();
 
     if ( !state.initialized ) {
