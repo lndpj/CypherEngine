611978d20a42f667c5dbeb781d6a74f7d88aaa7e
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-12 12:20:29 +0200

    Use subsystem error types in CypherRender_Shader.h

--- src/CypherEngine/CypherRender/CypherRender_Shader.h
+++ src/CypherEngine/CypherRender/CypherRender_Shader.h
@@ -46,14 +46,14 @@ void CypherRender_ShaderRegistryInit( shader_registry_t &shader_registry );
 
 void CypherRender_ShaderRegistryShutdown( shader_registry_t &shader_registry );
 
-error_code_t CypherRender_ShaderLoad( shader_registry_t &shader_registry, const char *name, const char *vertex_path, const char *fragment_path, shader_t **out_shader );
+render_error_t CypherRender_ShaderLoad( shader_registry_t &shader_registry, const char *name, const char *vertex_path, const char *fragment_path, shader_t **out_shader );
 
 shader_t *CypherRender_ShaderFind( shader_registry_t &registry, const char *name );
 
-error_code_t CypherRender_ShaderBind( const shader_t &shader );
+render_error_t CypherRender_ShaderBind( const shader_t &shader );
 
 void CypherRender_ShaderUnload( shader_t &shader );
 
-error_code_t CypherRender_ShaderSetMat4( const shader_t &shader, const char *uniform_name, const math::mat4_t &matrix );
+render_error_t CypherRender_ShaderSetMat4( const shader_t &shader, const char *uniform_name, const math::mat4_t &matrix );
 
 }       // namespace cypher::engine::render
