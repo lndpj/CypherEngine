effb104d2bee12fa4d5ddf2551bef161228abc90
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-12 12:20:27 +0200

    Rename error type in CypherCommon_Error.h

--- src/CypherEngine/CypherCommon/CypherCommon_Error.h
+++ src/CypherEngine/CypherCommon/CypherCommon_Error.h
@@ -29,7 +29,7 @@ enum class domain_t : u16 {
     COM_DOMAIN_MEMORY
 };
 
-enum class error_code_t : com_u8 {
+enum class common_error_t : com_u8 {
 	OK = 0,
     
 	ERR_FAILED,
@@ -50,39 +50,39 @@ enum class error_code_t : com_u8 {
 Common Error Helpers
 ================
 */
-constexpr bool CypherCommon_ErrorOk( const error_code_t code ) {
-	return code == error_code_t::OK;
+constexpr bool CypherCommon_ErrorOk( const common_error_t code ) {
+	return code == common_error_t::OK;
 }
 
-constexpr bool CypherCommon_ErrorFailed( const error_code_t code ) {
-	return code != error_code_t::OK;
+constexpr bool CypherCommon_ErrorFailed( const common_error_t code ) {
+	return code != common_error_t::OK;
 }
 
-constexpr inline const char *CypherCommon_ErrorName( const error_code_t code ) {
+constexpr inline const char *CypherCommon_ErrorName( const common_error_t code ) {
 	switch ( code ) {
-	case error_code_t::OK:
+	case common_error_t::OK:
 		return "OK";
-	case error_code_t::ERR_FAILED:
+	case common_error_t::ERR_FAILED:
 		return "COM_FAILED";
-	case error_code_t::ERR_INVALID_ARGUMENT:
+	case common_error_t::ERR_INVALID_ARGUMENT:
 		return "COM_INVALID_ARGUMENT";
-	case error_code_t::ERR_INVALID_STATE:
+	case common_error_t::ERR_INVALID_STATE:
 		return "COM_INVALID_STATE";
-	case error_code_t::ERR_INVALID_OPERATION:
+	case common_error_t::ERR_INVALID_OPERATION:
 		return "COM_INVALID_OPERATION";
-	case error_code_t::ERR_NOT_INIT:
+	case common_error_t::ERR_NOT_INIT:
 		return "COM_NOT_INITIALIZED";
-	case error_code_t::ERR_IS_INIT:
+	case common_error_t::ERR_IS_INIT:
 		return "COM_ALREADY_INITIALIZED";
-	case error_code_t::ERR_OUT_OF_MEMORY:
+	case common_error_t::ERR_OUT_OF_MEMORY:
 		return "COM_OUT_OF_MEMORY";
-	case error_code_t::ERR_NOT_FOUND:
+	case common_error_t::ERR_NOT_FOUND:
 		return "NOT_FOUND";
-	case error_code_t::ERR_UNSUPPORTED:
+	case common_error_t::ERR_UNSUPPORTED:
 		return "COM_UNSUPPORTED";
-	case error_code_t::ERR_IO_ERROR:
+	case common_error_t::ERR_IO_ERROR:
 		return "IO_ERROR";
-	case error_code_t::ERR_INTERNAL_ERROR:
+	case common_error_t::ERR_INTERNAL_ERROR:
 		return "COM_INTERNAL_ERROR";
 	default:
 		return "COM_UNKNOWN_ERROR";
