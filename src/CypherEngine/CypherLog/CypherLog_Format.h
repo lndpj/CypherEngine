06104ee759335d8f191545b818deacff9b7f7bf8
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-12 12:20:28 +0200

    Use subsystem error types in CypherLog_Format.h

--- src/CypherEngine/CypherLog/CypherLog_Format.h
+++ src/CypherEngine/CypherLog/CypherLog_Format.h
@@ -13,21 +13,21 @@ Formatting functions
 Used for formatting properly all of the logging system
 ================
 */
-error_code_t CypherLog_FormatRecord(
+log_error_t CypherLog_FormatRecord(
     const record_t &record,
     const sink_config_t &sink_config,
     const config_t &config,
     char *out_buffer,
     common::usize out_buffer_size );
 
-error_code_t CypherLog_FormatDetailed(
+log_error_t CypherLog_FormatDetailed(
     const record_t &record,
     const sink_config_t &sink_config,
     const config_t &config,
     char *out_buffer,
     const common::usize out_buffer_size );
 
-error_code_t CypherLog_FormatCompact(
+log_error_t CypherLog_FormatCompact(
     const record_t &record,
     const sink_config_t &sink_config,
     char *out_buffer,
