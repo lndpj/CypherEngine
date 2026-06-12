3aa6a6752f3fd2c4e468196a48ffcfcfb53084b2
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-12 17:35:17 +0200

    Add CypherPak umbrella header

---
+++ src/CypherEngine/CypherPak/CypherPak.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "CypherEngine/CypherPak/CypherPak_Compression.h"
+#include "CypherEngine/CypherPak/CypherPak_Error.h"
+#include "CypherEngine/CypherPak/CypherPak_Format.h"
+#include "CypherEngine/CypherPak/CypherPak_Reader.h"
+#include "CypherEngine/CypherPak/CypherPak_Types.h"
+#include "CypherEngine/CypherPak/CypherPak_Writer.h"
+
+namespace cypher::engine::pak
+{
+
+/*
+================
+CypherPak
+
+Runtime package archive backend used by the filesystem for .cypak mounts.
+Tools will use the writer API later to build deterministic content packages.
+================
+*/
+
+}       // namespace cypher::engine::pak
