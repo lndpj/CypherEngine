11b179e5223ca5dd961ad081ab17296db12e6e72
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-14 12:11:19 +0200

    Add filesystem header guard

--- src/CypherEngine/CypherFileSystem/CypherFileSystem.h
+++ src/CypherEngine/CypherFileSystem/CypherFileSystem.h
@@ -1,3 +1,6 @@
+#ifndef CYPHER_ENGINE_FILESYSTEM_H
+#define CYPHER_ENGINE_FILESYSTEM_H
+
 #pragma once
 
 #include "CypherEngine/CypherFileSystem/CypherFileSystem_Error.h"
@@ -330,3 +333,5 @@ fs_error_t CypherFileSystem_DumpMounts();
 fs_error_t CypherFileSystem_DumpStats();
 
 }       // namespace cypher::engine::fs
+
+#endif // CYPHER_ENGINE_FILESYSTEM_H
