c0811a1a474d9a0ae80213c26c5d3dc65c3437b6
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-20 12:37:40 +0200

    style: update CypherLog_Format.h naming

--- src/CypherEngine/CypherLog/CypherLog_Format.h
+++ src/CypherEngine/CypherLog/CypherLog_Format.h
@@ -18,25 +18,25 @@ Used for formatting properly all of the logging system
 */
 log_error_t CypherLog_FormatRecord(
     const record_t &record,
-    const sink_config_t &sink_config,
+    const sink_config_t &pSinkConfig,
     const config_t &config,
-    char *out_buffer,
-    common::usize out_buffer_size );
+    char *bufferOut,
+    common::usize nOutBufferSize );
 
 log_error_t CypherLog_FormatDetailed(
     const record_t &record,
-    const sink_config_t &sink_config,
+    const sink_config_t &pSinkConfig,
     const config_t &config,
-    char *out_buffer,
-    const common::usize out_buffer_size );
+    char *bufferOut,
+    const common::usize nOutBufferSize );
 
 log_error_t CypherLog_FormatCompact(
     const record_t &record,
-    const sink_config_t &sink_config,
-    char *out_buffer,
-    const common::usize out_buffer_size );
+    const sink_config_t &pSinkConfig,
+    char *bufferOut,
+    const common::usize nOutBufferSize );
 
-bool CypherLog_FormatTimestamp( const record_t &record, char *out_buffer, const common::usize out_buffer_size );
+bool CypherLog_FormatTimestamp( const record_t &record, char *bufferOut, const common::usize nOutBufferSize );
 
 const char *CypherLog_LevelColor( const level_t level );
 
