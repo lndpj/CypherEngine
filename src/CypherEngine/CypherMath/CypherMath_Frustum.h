7754595ac61cb4c18d60e54b7b716ce11cb04778
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-20 12:37:41 +0200

    style: update CypherMath_Frustum.h naming

--- src/CypherEngine/CypherMath/CypherMath_Frustum.h
+++ src/CypherEngine/CypherMath/CypherMath_Frustum.h
@@ -7,13 +7,13 @@
 
 namespace cypher::engine::math
 {
- 
-frustum_t CypherMath_FrustumFromProjectionView( const mat4_t &projection_view );
-    
+
+frustum_t CypherMath_FrustumFromProjectionView( const mat4_t &projectionView );
+
 bool CypherMath_FrustumContainsPoint( const frustum_t &frustum, const vec3_t &point );
 
-bool CypherMath_FrustumIntersectsBounds( const frustum_t &frustum, const bounds_t &bounds );  
-    
+bool CypherMath_FrustumIntersectsBounds( const frustum_t &frustum, const bounds_t &bounds );
+
 }       // namespace cypher::engine::math
 
 #endif // CYPHER_ENGINE_MATH_FRUSTUM_H
