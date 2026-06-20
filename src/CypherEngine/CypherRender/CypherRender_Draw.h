6e3a4ff15e07eb67617b1fb0031f1e1cbe9480f6
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-20 12:37:42 +0200

    style: update CypherRender_Draw.h naming

--- src/CypherEngine/CypherRender/CypherRender_Draw.h
+++ src/CypherEngine/CypherRender/CypherRender_Draw.h
@@ -10,11 +10,11 @@
 
 namespace cypher::engine::render
 {
-    
+
 constexpr common::u32 CYPHER_RENDER_DRAW_ITEMS_LIST_MAX = 16384u;
 
 struct draw_item_t {
-    math::mat4_t model_matrix{};
+    math::mat4_t modelMatrix{};
 
     mesh_t    *mesh{};
     shader_t  *shader{};
@@ -22,19 +22,19 @@ struct draw_item_t {
 
 struct draw_list_t {
     draw_item_t *items{ nullptr };
-    common::u32 item_count{ 0u };
-    common::u32 item_capacity{ 0u };
+    common::u32 nItemCount{ 0u };
+    common::u32 nItemCapacity{ 0u };
 };
 
 render_error_t CypherRender_DrawItem( const draw_item_t &item, const camera_t &camera );
 
-void CypherRender_DrawListInit( draw_list_t &draw_list, draw_item_t *items, common::u32 item_capacity );
+void CypherRender_DrawListInit( draw_list_t &drawList, draw_item_t *items, common::u32 nItemCapacity );
 
-void CypherRender_DrawListClear( draw_list_t &draw_list );
+void CypherRender_DrawListClear( draw_list_t &drawList );
 
-render_error_t CypherRender_DrawListSubmit( draw_list_t &draw_list, const draw_item_t &item );
+render_error_t CypherRender_DrawListSubmit( draw_list_t &drawList, const draw_item_t &item );
 
-render_error_t CypherRender_DrawListDraw( const draw_list_t &draw_list, const camera_t &camera );
+render_error_t CypherRender_DrawListDraw( const draw_list_t &drawList, const camera_t &camera );
 
 }       // namespace cypher::engine::render
 
