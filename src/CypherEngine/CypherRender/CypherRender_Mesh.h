f34b2d00644267f3f7bc298f9c0079f1f8b08ab1
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-20 19:51:03 +0200

    chore: update src/CypherEngine/CypherRender/CypherRender_Mesh.h

--- src/CypherEngine/CypherRender/CypherRender_Mesh.h
+++ src/CypherEngine/CypherRender/CypherRender_Mesh.h
@@ -3,8 +3,8 @@
 
 #pragma once
 
-#include "CypherEngine/CypherMath/CypherMath_Types.h"
-#include "CypherEngine/CypherRender/CypherRender_Error.h"
+#include "CypherMath_Types.h"
+#include "CypherRender_Error.h"
 
 namespace cypher::engine::render
 {
