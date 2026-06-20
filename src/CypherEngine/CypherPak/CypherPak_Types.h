80ea8f42cfc909d66bc4f2ec938eae4e8264a7af
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-20 12:37:42 +0200

    style: update CypherPak_Types.h naming

--- src/CypherEngine/CypherPak/CypherPak_Types.h
+++ src/CypherEngine/CypherPak/CypherPak_Types.h
@@ -37,37 +37,37 @@ enum pak_verify_flags_t : common::u32 {
 };
 
 struct pak_file_info_t {
-    char virtual_path[CYPHER_PAK_MAX_PATH_LENGTH]{};
+    char szVirtualPath[CYPHER_PAK_MAX_PATH_LENGTH]{};
     pak_file_index_t index{ CYPHER_PAK_INVALID_FILE_INDEX };
-    common::u64 data_offset{ 0u };
-    common::u64 stored_size{ 0u };
-    common::u64 unpacked_size{ 0u };
-    common::u64 modified_time_utc{ 0u };
-    common::u64 content_hash{ 0u };
-    common::u32 path_hash{ 0u };
+    common::u64 nDataOffset{ 0u };
+    common::u64 nStoredSize{ 0u };
+    common::u64 nUnpackedSize{ 0u };
+    common::u64 modifiedTimeUtc{ 0u };
+    common::u64 contentHash{ 0u };
+    common::u32 szPathHash{ 0u };
     pak_compression_t compression{ pak_compression_t::NONE };
     common::u32 flags{ CYPHER_PAK_ENTRY_NONE };
 };
 
 struct pak_stats_t {
-    common::u64 file_count{ 0u };
-    common::u64 archive_size{ 0u };
-    common::u64 compressed_file_count{ 0u };
-    common::u64 stored_data_size{ 0u };
-    common::u64 unpacked_data_size{ 0u };
-    common::u64 read_count{ 0u };
-    common::u64 bytes_read{ 0u };
+    common::u64 nFileCount{ 0u };
+    common::u64 nArchiveSize{ 0u };
+    common::u64 nCompressedFileCount{ 0u };
+    common::u64 nStoredDataSize{ 0u };
+    common::u64 nUnpackedDataSize{ 0u };
+    common::u64 nReadCount{ 0u };
+    common::u64 nBytesRead{ 0u };
 };
 
 struct pak_reader_t {
     pak_handle_t handle{ CYPHER_PAK_INVALID_HANDLE };
-    void *native_file{ nullptr };
-    char archive_path[CYPHER_PAK_MAX_ARCHIVE_PATH_LENGTH]{};
+    void *pNativeFile{ nullptr };
+    char szArchivePath[CYPHER_PAK_MAX_ARCHIVE_PATH_LENGTH]{};
     pak_header_t header{};
     pak_disk_file_entry_t *entries{ nullptr };
-    char *string_table{ nullptr };
-    common::u64 string_table_size{ 0u };
-    common::u32 file_count{ 0u };
+    char *stringTable{ nullptr };
+    common::u64 nStringTableSize{ 0u };
+    common::u32 nFileCount{ 0u };
     common::u32 flags{ CYPHER_PAK_OPEN_NONE };
     pak_stats_t stats{};
     bool open{ false };
@@ -75,20 +75,20 @@ struct pak_reader_t {
 
 struct pak_writer_t {
     pak_handle_t handle{ CYPHER_PAK_INVALID_HANDLE };
-    void *native_file{ nullptr };
-    void *builder_state{ nullptr };
-    char archive_path[CYPHER_PAK_MAX_ARCHIVE_PATH_LENGTH]{};
+    void *pNativeFile{ nullptr };
+    void *pBuilderState{ nullptr };
+    char szArchivePath[CYPHER_PAK_MAX_ARCHIVE_PATH_LENGTH]{};
     common::u32 flags{ 0u };
-    common::u32 data_alignment{ CYPHER_PAK_DATA_ALIGNMENT };
-    pak_compression_t default_compression{ pak_compression_t::NONE };
-    common::u32 file_count{ 0u };
+    common::u32 nDataAlignment{ CYPHER_PAK_DATA_ALIGNMENT };
+    pak_compression_t defaultCompression{ pak_compression_t::NONE };
+    common::u32 nFileCount{ 0u };
     bool open{ false };
     bool finalized{ false };
 };
 
 struct pak_source_file_t {
-    const char *virtual_path{ nullptr };
-    const char *physical_path{ nullptr };
+    const char *szVirtualPath{ nullptr };
+    const char *szPhysicalPath{ nullptr };
     pak_compression_t compression{ pak_compression_t::NONE };
     common::u32 flags{ CYPHER_PAK_ENTRY_NONE };
 };
