51f39c4e73d42d97a4721226b2d3251b4221947b
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-20 12:37:41 +0200

    style: update CypherMath_Mat.h naming

--- src/CypherEngine/CypherMath/CypherMath_Mat.h
+++ src/CypherEngine/CypherMath/CypherMath_Mat.h
@@ -26,7 +26,7 @@ World convention:
     -Z forward
 ================
 */
-    
+
 constexpr inline common::u32 CypherMath_Mat4Index( const common::u32 column, const common::u32 row ) {
     return column * 4u + row;
 }
@@ -45,7 +45,7 @@ mat4_t CypherMath_Mat4Identity();
 
 mat4_t CypherMath_Mat4Multiply( const mat4_t &m1, const mat4_t &m2 );
 
-mat4_t CypherMath_Mat4Translation( const vec3_t &translation_vector );
+mat4_t CypherMath_Mat4Translation( const vec3_t &translationVector );
 
 mat4_t CypherMath_Mat4Scale( const vec3_t &scale );
 
@@ -65,9 +65,9 @@ vec3_t CypherMath_Mat4TransformPoint( const mat4_t &m, const vec3_t &v );
 
 vec3_t CypherMath_Mat4TransformDirection( const mat4_t &m, const vec3_t &v );
 
-mat4_t CypherMath_Mat4Perspective( common::f32 fov_y_radians, common::f32 aspect_ratio, common::f32 near_z, common::f32 far_z );
+mat4_t CypherMath_Mat4Perspective( common::f32 fovYRadians, common::f32 aspectRatio, common::f32 nearZ, common::f32 farZ );
 
-mat4_t CypherMath_Mat4Ortho( const common::f32 left, const common::f32 right, const common::f32 bottom, const common::f32 top, const common::f32 near_z, const common::f32 far_z );
+mat4_t CypherMath_Mat4Ortho( const common::f32 left, const common::f32 right, const common::f32 bottom, const common::f32 top, const common::f32 nearZ, const common::f32 farZ );
 
 mat4_t CypherMath_Mat4LookAt( const vec3_t &eye, const vec3_t &target, const vec3_t &up );
 
