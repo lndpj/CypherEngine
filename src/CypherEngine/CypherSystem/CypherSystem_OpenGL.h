eab37e57b20da470e3247a18cf7c68c48a4ac6a5
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-20 12:37:43 +0200

    style: update CypherSystem_OpenGL.h naming

--- src/CypherEngine/CypherSystem/CypherSystem_OpenGL.h
+++ src/CypherEngine/CypherSystem/CypherSystem_OpenGL.h
@@ -16,13 +16,13 @@ macOS is capped at OpenGL 4.1 core, while Windows/Linux can request newer GL.
 ================
 */
 #ifdef CYPHER_PLATFORM_MACOS
-    constexpr int SYS_GL_CONTEXT_MAJOR = 4;   
+    constexpr int SYS_GL_CONTEXT_MAJOR = 4;
     constexpr int SYS_GL_CONTEXT_MINOR = 1;
     constexpr int SYS_GL_CONTEXT_FLAGS = SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG;
 #elif defined( CYPHER_PLATFORM_WINDOWS ) || defined( CYPHER_PLATFORM_LINUX )
     constexpr int SYS_GL_CONTEXT_MAJOR = 4;
     constexpr int SYS_GL_CONTEXT_MINOR = 5;
-    constexpr int SYS_GL_CONTEXT_FLAGS = 0; 
+    constexpr int SYS_GL_CONTEXT_FLAGS = 0;
 #else
     // Conservative fallback for unknown platforms.
     constexpr int SYS_GL_SYS_GL_CONTEXT_MAJOR = 4;
