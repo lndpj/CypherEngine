ccd9f684bd3ffdd4a66c8076914b727cecf55ec7
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-20 12:37:41 +0200

    style: update CypherMath_Vec.cpp naming

--- src/CypherEngine/CypherMath/CypherMath_Vec.cpp
+++ src/CypherEngine/CypherMath/CypherMath_Vec.cpp
@@ -7,10 +7,10 @@
    Last Modified: 2026-05-22 12:27:31
    ---------------------------------------------------------------------
    Description:
-       
+
    ---------------------------------------------------------------------
-   License: 
-   Company: 
+   License:
+   Company:
    Version: 0.1.0
  ======================================================================
                                                                        */
@@ -32,30 +32,30 @@ common::f32 CypherMath_Vec2Length( const vec2_t &v ) {
 }
 
 common::f32 CypherMath_Vec2Distance( const vec2_t &v1, const vec2_t &v2 ) {
-    return CypherMath_Vec2Length( CypherMath_Vec2Sub( v1, v2 ) );   
+    return CypherMath_Vec2Length( CypherMath_Vec2Sub( v1, v2 ) );
 }
 
 vec2_t CypherMath_Vec2Normalize(const vec2_t &v ) {
-    const common::f32 vec_len = CypherMath_Vec2Length( v );
-    
+    const common::f32 nVecLen = CypherMath_Vec2Length( v );
+
     // @NOTE: In case the length of the vector is very very small we count it as 0.
-    if ( vec_len <= MATH_EPSILON_F ) {
+    if ( nVecLen <= MATH_EPSILON_F ) {
         return vec2_t{};
     }
-    
-    return CypherMath_Vec2Scale( v, 1.0f / vec_len );
+
+    return CypherMath_Vec2Scale( v, 1.0f / nVecLen );
 }
 
 common::f32 CypherMath_Vec2NormalizeLength( vec2_t &v ) {
-    const common::f32 vec_len = CypherMath_Vec2Length( v );
-    
-    if ( vec_len <= MATH_EPSILON_F ) {
+    const common::f32 nVecLen = CypherMath_Vec2Length( v );
+
+    if ( nVecLen <= MATH_EPSILON_F ) {
         v = vec2_t{};
         return 0.0f;
     }
-    
-    v = CypherMath_Vec2Scale( v, 1.0f / vec_len );
-    return vec_len;
+
+    v = CypherMath_Vec2Scale( v, 1.0f / nVecLen );
+    return nVecLen;
 }
 
 bool CypherMath_Vec2Near( const vec2_t &v1, const vec2_t &v2, common::f32 epsilon ) {
@@ -64,7 +64,7 @@ bool CypherMath_Vec2Near( const vec2_t &v1, const vec2_t &v2, common::f32 epsilo
 }
 
 vec2_t CypherMath_Vec2Lerp( const vec2_t &v1, const vec2_t &v2, common::f32 t ) {
-    
+
     return vec2_t{
         v1.x + ( v2.x - v1.x ) * t,
         v1.y + ( v2.y - v1.y ) * t
@@ -99,52 +99,52 @@ vec3_t CypherMath_Vec3Cross( const vec3_t &v1, const vec3_t &v2 )
     };
 }
 
-common::f32 CypherMath_Vec3Length( const vec3_t &v ) 
+common::f32 CypherMath_Vec3Length( const vec3_t &v )
 {
     return std::sqrt( CypherMath_Vec3Dot( v, v ) );
 }
 
 common::f32 CypherMath_Vec3Distance( const vec3_t &v1, const vec3_t &v2 ) {
-    return CypherMath_Vec3Length( CypherMath_Vec3Sub( v1, v2 ) );   
+    return CypherMath_Vec3Length( CypherMath_Vec3Sub( v1, v2 ) );
 }
 
 
 
 vec3_t CypherMath_Vec3Normalize( const vec3_t &v ) {
-    const common::f32 vec_len = CypherMath_Vec3Length( v );
-    
-    if ( vec_len <= MATH_EPSILON_F ) {
+    const common::f32 nVecLen = CypherMath_Vec3Length( v );
+
+    if ( nVecLen <= MATH_EPSILON_F ) {
         return vec3_t{};
     }
-    
-    return CypherMath_Vec3Scale( v, 1.0f / vec_len );
+
+    return CypherMath_Vec3Scale( v, 1.0f / nVecLen );
 }
 
 common::f32 CypherMath_Vec3NormalizeLength( vec3_t &v ) {
-    const common::f32 vec_len = CypherMath_Vec3Length( v );
-    
-    if ( vec_len <= MATH_EPSILON_F ) {
+    const common::f32 nVecLen = CypherMath_Vec3Length( v );
+
+    if ( nVecLen <= MATH_EPSILON_F ) {
         v = vec3_t{};
         return 0.0f;
     }
-    
-    v = CypherMath_Vec3Scale( v, ( 1.0f / vec_len ) );
-    
-    return vec_len;
+
+    v = CypherMath_Vec3Scale( v, ( 1.0f / nVecLen ) );
+
+    return nVecLen;
 }
 
 bool CypherMath_Vec3Near( const vec3_t &v1, const vec3_t &v2, common::f32 epsilon ) {
     return ( std::fabs( v1.x - v2.x ) <= epsilon &&
              std::fabs( v1.y - v2.y ) <= epsilon &&
              std::fabs( v1.z - v2.z ) <= epsilon );
-       
+
 }
 
 vec3_t CypherMath_Vec3Lerp( const vec3_t &v1, const vec3_t &v2, common::f32 t ) {
     return vec3_t{
         v1.x + ( v2.x - v1.x ) * t,
         v1.y + ( v2.y - v1.y ) * t,
-        v1.z + ( v2.z - v1.z ) * t  
+        v1.z + ( v2.z - v1.z ) * t
     };
 }
 
@@ -165,17 +165,17 @@ vec3_t CypherMath_Vec3Max( const vec3_t &v1, const vec3_t &v2 ) {
 }
 
 vec3_t CypherMath_Vec3Reflect( const vec3_t &v, const vec3_t &normal ) {
-    common::f32 reflect_dot = CypherMath_Vec3Dot( v, normal );
-    return CypherMath_Vec3Sub( v, CypherMath_Vec3Scale( normal, reflect_dot * 2.0f ) );
+    common::f32 reflectDot = CypherMath_Vec3Dot( v, normal );
+    return CypherMath_Vec3Sub( v, CypherMath_Vec3Scale( normal, reflectDot * 2.0f ) );
 }
 
 vec3_t CypherMath_Vec3Project( const vec3_t &v, const vec3_t &onto ) {
-    common::f32 norm_onto = CypherMath_Vec3LengthSquared( onto );
-    if ( norm_onto <= MATH_EPSILON_F ) {
+    common::f32 normOnto = CypherMath_Vec3LengthSquared( onto );
+    if ( normOnto <= MATH_EPSILON_F ) {
         return vec3_t{};
-    }   
-    
-    common::f32 scale = CypherMath_Vec3Dot( v, onto ) / norm_onto;
+    }
+
+    common::f32 scale = CypherMath_Vec3Dot( v, onto ) / normOnto;
     return CypherMath_Vec3Scale( onto, scale );
 }
 
@@ -193,19 +193,19 @@ common::f32 CypherMath_Vec4Length( const vec4_t &v ) {
 }
 
 vec4_t CypherMath_Vec4Normalize( const vec4_t &v ) {
-    const common::f32 vec_len = CypherMath_Vec4Length( v );
-    
-    if ( vec_len <= MATH_EPSILON_F ) {
+    const common::f32 nVecLen = CypherMath_Vec4Length( v );
+
+    if ( nVecLen <= MATH_EPSILON_F ) {
         return vec4_t{};
     }
-    return CypherMath_Vec4Scale( v, 1.0f / vec_len );
+    return CypherMath_Vec4Scale( v, 1.0f / nVecLen );
 }
 
 bool CypherMath_Vec4Near( const vec4_t &v1, const vec4_t &v2, common::f32 epsilon ) {
     return ( std::fabs( v1.x - v2.x ) <= epsilon &&
              std::fabs( v1.y - v2.y ) <= epsilon &&
              std::fabs( v1.z - v2.z ) <= epsilon &&
-             std::fabs( v1.w - v2.w ) <= epsilon 
+             std::fabs( v1.w - v2.w ) <= epsilon
              );
 }
 
@@ -214,7 +214,7 @@ vec4_t CypherMath_Vec4Lerp( const vec4_t &v1, const vec4_t &v2, common::f32 t ) 
         v1.x + ( v2.x - v1.x ) * t,
         v1.y + ( v2.y - v1.y ) * t,
         v1.z + ( v2.z - v1.z ) * t,
-        v1.w + ( v2.w - v1.w ) * t  
+        v1.w + ( v2.w - v1.w ) * t
     };
 }
 
