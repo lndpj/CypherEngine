e80323906582367b3ce6a181863aef2a48a9a387
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-20 12:37:42 +0200

    style: update CypherRender_Camera.h naming

--- src/CypherEngine/CypherRender/CypherRender_Camera.h
+++ src/CypherEngine/CypherRender/CypherRender_Camera.h
@@ -8,37 +8,37 @@
 
 namespace cypher::engine::render
 {
-    
+
 enum camera_projection_mode_t {
     PERSPECTIVE,
     ORTOGRAPHIC
-};  
+};
 
 struct camera_desc_t {
-    camera_projection_mode_t camera_projection_mode{ camera_projection_mode_t::PERSPECTIVE };
-    common::f32 fov_y_radians{ CYPHER_RENDER_DEFAULT_FOV_Y_RADIANS };
-    common::f32 aspect_ratio{ CYPHER_RENDER_DEFAULT_ASPECT_RATIO };
-    common::f32 near_z{ CYPHER_RENDER_DEFAULT_NEAR_Z };
-    common::f32 far_z{ CYPHER_RENDER_DEFAULT_FAR_Z };
+    camera_projection_mode_t cameraProjectionMode{ camera_projection_mode_t::PERSPECTIVE };
+    common::f32 fovYRadians{ CYPHER_RENDER_DEFAULT_FOV_Y_RADIANS };
+    common::f32 aspectRatio{ CYPHER_RENDER_DEFAULT_ASPECT_RATIO };
+    common::f32 nearZ{ CYPHER_RENDER_DEFAULT_NEAR_Z };
+    common::f32 farZ{ CYPHER_RENDER_DEFAULT_FAR_Z };
 };
 
 struct camera_t {
     math::vec3_t position{};
     math::quat_t orientation{};
-    
-    camera_desc_t camera_desc{};
-    
+
+    camera_desc_t cameraDesc{};
+
     math::mat4_t view{};
     math::mat4_t projection{};
-    math::mat4_t projection_view{};
+    math::mat4_t projectionView{};
     math::frustum_t frustum{};
 };
- 
-void CypherRender_CameraInit( camera_t &camera, const camera_desc_t &camera_desc );
-    
-void CypherRender_CameraUpdateMatrices( camera_t &camera );    
 
-void CypherRender_CameraSetPerspective( camera_t &camera, common::f32 fov_y_radians, common::f32 aspect_ration, common::f32 near_z, common::f32 far_z );
+void CypherRender_CameraInit( camera_t &camera, const camera_desc_t &cameraDesc );
+
+void CypherRender_CameraUpdateMatrices( camera_t &camera );
+
+void CypherRender_CameraSetPerspective( camera_t &camera, common::f32 fovYRadians, common::f32 aspectRation, common::f32 nearZ, common::f32 farZ );
 
 void CypherRender_CameraSetTransform( camera_t &camera, const math::vec3_t &position, const math::quat_t &orientation );
 
@@ -47,7 +47,7 @@ void CypherRender_CameraSetPosition( camera_t &camera, const math::vec3_t &posit
 void CypherRender_CameraSetOrientation( camera_t &camera, const math::quat_t &orientation );
 
 void CypherRender_CameraSetPerspectiveMode( camera_t &camera, camera_projection_mode_t &mode );
-    
+
 }       // namespace cypher::engine::render
 
 #endif // CYPHER_ENGINE_RENDER_CAMERA_H
