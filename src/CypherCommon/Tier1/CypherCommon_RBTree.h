32477703cdb4f9b7351a7b23eec5d996793038a0
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-22 14:01:54 +0200

    chore: add src/CypherCommon/Tier1/CypherCommon_RBTree.h

---
+++ src/CypherCommon/Tier1/CypherCommon_RBTree.h
@@ -0,0 +1,21 @@
+#ifndef CYPHER_COMMON_TIER1_RBTREE_H
+#define CYPHER_COMMON_TIER1_RBTREE_H
+#pragma once
+
+/*
+================
+CypherCommon RB Tree
+
+Red-black tree declarations.
+================
+*/
+
+namespace cypher::common
+{
+
+template <typename key_t, typename value_t>
+struct rb_tree_t;
+
+} // namespace cypher::common
+
+#endif // CYPHER_COMMON_TIER1_RBTREE_H
