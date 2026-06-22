646232636a327dc16ed8dc7fed46cc1f4f0a8878
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-22 14:01:53 +0200

    chore: add src/CypherCommon/Tier1/CypherCommon_Delegate.h

---
+++ src/CypherCommon/Tier1/CypherCommon_Delegate.h
@@ -0,0 +1,21 @@
+#ifndef CYPHER_COMMON_TIER1_DELEGATE_H
+#define CYPHER_COMMON_TIER1_DELEGATE_H
+#pragma once
+
+/*
+================
+CypherCommon Delegate
+
+Delegate declarations.
+================
+*/
+
+namespace cypher::common
+{
+
+template <typename signature_t>
+class delegate_t;
+
+} // namespace cypher::common
+
+#endif // CYPHER_COMMON_TIER1_DELEGATE_H
