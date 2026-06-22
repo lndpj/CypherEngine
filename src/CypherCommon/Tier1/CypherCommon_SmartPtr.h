aa1853987573aa3317cd98c2e439ce9c80cea52f
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-22 14:01:55 +0200

    chore: add src/CypherCommon/Tier1/CypherCommon_SmartPtr.h

---
+++ src/CypherCommon/Tier1/CypherCommon_SmartPtr.h
@@ -0,0 +1,24 @@
+#ifndef CYPHER_COMMON_TIER1_SMARTPTR_H
+#define CYPHER_COMMON_TIER1_SMARTPTR_H
+#pragma once
+
+/*
+================
+CypherCommon Smart Ptr
+
+Engine smart pointer declarations.
+================
+*/
+
+namespace cypher::common
+{
+
+template <typename type_t>
+class smart_ptr_t;
+
+template <typename type_t>
+class weak_ptr_t;
+
+} // namespace cypher::common
+
+#endif // CYPHER_COMMON_TIER1_SMARTPTR_H
