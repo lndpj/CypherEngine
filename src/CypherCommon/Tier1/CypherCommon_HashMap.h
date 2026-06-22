1bc74f2d40b50f68073874f3c55efe2e024eca6d
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-22 14:01:53 +0200

    chore: add src/CypherCommon/Tier1/CypherCommon_HashMap.h

---
+++ src/CypherCommon/Tier1/CypherCommon_HashMap.h
@@ -0,0 +1,21 @@
+#ifndef CYPHER_COMMON_TIER1_HASHMAP_H
+#define CYPHER_COMMON_TIER1_HASHMAP_H
+#pragma once
+
+/*
+================
+CypherCommon Hash Map
+
+Hash-map declarations.
+================
+*/
+
+namespace cypher::common
+{
+
+template <typename key_t, typename value_t>
+struct hash_map_t;
+
+} // namespace cypher::common
+
+#endif // CYPHER_COMMON_TIER1_HASHMAP_H
