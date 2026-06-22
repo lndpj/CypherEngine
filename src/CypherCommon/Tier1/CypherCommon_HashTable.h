c7aeb03d8857df4b5f3c4f4b2d83b6b37fec6346
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-22 14:01:53 +0200

    chore: add src/CypherCommon/Tier1/CypherCommon_HashTable.h

---
+++ src/CypherCommon/Tier1/CypherCommon_HashTable.h
@@ -0,0 +1,21 @@
+#ifndef CYPHER_COMMON_TIER1_HASHTABLE_H
+#define CYPHER_COMMON_TIER1_HASHTABLE_H
+#pragma once
+
+/*
+================
+CypherCommon Hash Table
+
+Hash table declarations.
+================
+*/
+
+namespace cypher::common
+{
+
+template <typename key_t, typename value_t>
+struct hash_table_t;
+
+} // namespace cypher::common
+
+#endif // CYPHER_COMMON_TIER1_HASHTABLE_H
