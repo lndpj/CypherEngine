538b928dcfd0384f007daf649c05021e506c312d
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-22 14:01:53 +0200

    chore: add src/CypherCommon/Tier1/CypherCommon_HashSet.h

---
+++ src/CypherCommon/Tier1/CypherCommon_HashSet.h
@@ -0,0 +1,21 @@
+#ifndef CYPHER_COMMON_TIER1_HASHSET_H
+#define CYPHER_COMMON_TIER1_HASHSET_H
+#pragma once
+
+/*
+================
+CypherCommon Hash Set
+
+Hash-set declarations.
+================
+*/
+
+namespace cypher::common
+{
+
+template <typename key_t>
+struct hash_set_t;
+
+} // namespace cypher::common
+
+#endif // CYPHER_COMMON_TIER1_HASHSET_H
