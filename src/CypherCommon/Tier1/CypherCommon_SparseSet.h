b1ab6ab1474ed5927cf6982bc21ed41b41ac58c7
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-22 14:01:55 +0200

    chore: add src/CypherCommon/Tier1/CypherCommon_SparseSet.h

---
+++ src/CypherCommon/Tier1/CypherCommon_SparseSet.h
@@ -0,0 +1,21 @@
+#ifndef CYPHER_COMMON_TIER1_SPARSESET_H
+#define CYPHER_COMMON_TIER1_SPARSESET_H
+#pragma once
+
+/*
+================
+CypherCommon Sparse Set
+
+Sparse-set declarations for entity/component-style storage.
+================
+*/
+
+namespace cypher::common
+{
+
+template <typename key_t>
+struct sparse_set_t;
+
+} // namespace cypher::common
+
+#endif // CYPHER_COMMON_TIER1_SPARSESET_H
