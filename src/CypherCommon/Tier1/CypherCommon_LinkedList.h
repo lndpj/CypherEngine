a916eb5312e8cf48125d51a0c1d86b2155a23795
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-22 14:01:54 +0200

    chore: add src/CypherCommon/Tier1/CypherCommon_LinkedList.h

---
+++ src/CypherCommon/Tier1/CypherCommon_LinkedList.h
@@ -0,0 +1,24 @@
+#ifndef CYPHER_COMMON_TIER1_LINKEDLIST_H
+#define CYPHER_COMMON_TIER1_LINKEDLIST_H
+#pragma once
+
+/*
+================
+CypherCommon Linked List
+
+Linked list declarations.
+================
+*/
+
+namespace cypher::common
+{
+
+template <typename type_t>
+struct linked_list_t;
+
+template <typename type_t>
+struct linked_list_node_t;
+
+} // namespace cypher::common
+
+#endif // CYPHER_COMMON_TIER1_LINKEDLIST_H
