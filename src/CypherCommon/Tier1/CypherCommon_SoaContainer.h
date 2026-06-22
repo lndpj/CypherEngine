a2ed8524ecfdfb5795a37f2b8b6daac4bfe6cfe5
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-22 14:01:55 +0200

    chore: add src/CypherCommon/Tier1/CypherCommon_SoaContainer.h

---
+++ src/CypherCommon/Tier1/CypherCommon_SoaContainer.h
@@ -0,0 +1,21 @@
+#ifndef CYPHER_COMMON_TIER1_SOACONTAINER_H
+#define CYPHER_COMMON_TIER1_SOACONTAINER_H
+#pragma once
+
+/*
+================
+CypherCommon SOA Container
+
+Structure-of-arrays container declarations.
+================
+*/
+
+namespace cypher::common
+{
+
+template <typename layout_t>
+struct soa_container_t;
+
+} // namespace cypher::common
+
+#endif // CYPHER_COMMON_TIER1_SOACONTAINER_H
