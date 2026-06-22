6d0a748a00f8104148d3cffd207475c9bd0304cb
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-22 14:01:53 +0200

    chore: add src/CypherCommon/Tier1/CypherCommon_Dictionary.h

---
+++ src/CypherCommon/Tier1/CypherCommon_Dictionary.h
@@ -0,0 +1,23 @@
+#ifndef CYPHER_COMMON_TIER1_DICTIONARY_H
+#define CYPHER_COMMON_TIER1_DICTIONARY_H
+#pragma once
+
+/*
+================
+CypherCommon Dictionary
+
+String-keyed dictionary declarations.
+================
+*/
+
+#include "CypherCommon_Tier0.h"
+
+namespace cypher::common
+{
+
+template <typename value_t>
+struct dictionary_t;
+
+} // namespace cypher::common
+
+#endif // CYPHER_COMMON_TIER1_DICTIONARY_H
