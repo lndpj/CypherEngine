27d43fb55bae3eb1afc4fe352447362d6e376742
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-22 14:01:53 +0200

    chore: add src/CypherCommon/Tier1/CypherCommon_Function.h

---
+++ src/CypherCommon/Tier1/CypherCommon_Function.h
@@ -0,0 +1,21 @@
+#ifndef CYPHER_COMMON_TIER1_FUNCTION_H
+#define CYPHER_COMMON_TIER1_FUNCTION_H
+#pragma once
+
+/*
+================
+CypherCommon Function
+
+Small callable wrapper declarations.
+================
+*/
+
+namespace cypher::common
+{
+
+template <typename signature_t>
+class function_t;
+
+} // namespace cypher::common
+
+#endif // CYPHER_COMMON_TIER1_FUNCTION_H
