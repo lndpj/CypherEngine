ee10c7869ad0bb24d2f46cdbc9b17e5481678c44
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-22 14:01:54 +0200

    chore: add src/CypherCommon/Tier1/CypherCommon_Optional.h

---
+++ src/CypherCommon/Tier1/CypherCommon_Optional.h
@@ -0,0 +1,21 @@
+#ifndef CYPHER_COMMON_TIER1_OPTIONAL_H
+#define CYPHER_COMMON_TIER1_OPTIONAL_H
+#pragma once
+
+/*
+================
+CypherCommon Optional
+
+Optional value declarations.
+================
+*/
+
+namespace cypher::common
+{
+
+template <typename type_t>
+struct optional_t;
+
+} // namespace cypher::common
+
+#endif // CYPHER_COMMON_TIER1_OPTIONAL_H
