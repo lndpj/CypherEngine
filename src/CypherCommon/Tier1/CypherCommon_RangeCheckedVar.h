bf5009f623742bb1a5cb48d78c367ca2a4c8afb1
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-22 14:01:54 +0200

    chore: add src/CypherCommon/Tier1/CypherCommon_RangeCheckedVar.h

---
+++ src/CypherCommon/Tier1/CypherCommon_RangeCheckedVar.h
@@ -0,0 +1,21 @@
+#ifndef CYPHER_COMMON_TIER1_RANGECHECKEDVAR_H
+#define CYPHER_COMMON_TIER1_RANGECHECKEDVAR_H
+#pragma once
+
+/*
+================
+CypherCommon Range Checked Var
+
+Range-checked variable declarations.
+================
+*/
+
+namespace cypher::common
+{
+
+template <typename type_t>
+struct range_checked_var_t;
+
+} // namespace cypher::common
+
+#endif // CYPHER_COMMON_TIER1_RANGECHECKEDVAR_H
