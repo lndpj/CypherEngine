692082d696054f2a26ce34353e9f3653d5c2ed9c
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-22 14:01:54 +0200

    chore: add src/CypherCommon/Tier1/CypherCommon_PriorityQueue.h

---
+++ src/CypherCommon/Tier1/CypherCommon_PriorityQueue.h
@@ -0,0 +1,21 @@
+#ifndef CYPHER_COMMON_TIER1_PRIORITYQUEUE_H
+#define CYPHER_COMMON_TIER1_PRIORITYQUEUE_H
+#pragma once
+
+/*
+================
+CypherCommon Priority Queue
+
+Priority queue declarations.
+================
+*/
+
+namespace cypher::common
+{
+
+template <typename type_t>
+struct priority_queue_t;
+
+} // namespace cypher::common
+
+#endif // CYPHER_COMMON_TIER1_PRIORITYQUEUE_H
