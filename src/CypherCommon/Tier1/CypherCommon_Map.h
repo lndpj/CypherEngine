81d5dd07ffe901beab0bb494c50033cb14a4df8c
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-22 14:01:54 +0200

    chore: add src/CypherCommon/Tier1/CypherCommon_Map.h

---
+++ src/CypherCommon/Tier1/CypherCommon_Map.h
@@ -0,0 +1,21 @@
+#ifndef CYPHER_COMMON_TIER1_MAP_H
+#define CYPHER_COMMON_TIER1_MAP_H
+#pragma once
+
+/*
+================
+CypherCommon Map
+
+Ordered key/value container declarations.
+================
+*/
+
+namespace cypher::common
+{
+
+template <typename key_t, typename value_t>
+struct map_t;
+
+} // namespace cypher::common
+
+#endif // CYPHER_COMMON_TIER1_MAP_H
