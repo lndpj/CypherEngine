dbcd17573ea5df87b7fb747edac2b95660fbcf63
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-06-22 14:01:55 +0200

    chore: add src/CypherCommon/Tier1/CypherCommon_Set.h

---
+++ src/CypherCommon/Tier1/CypherCommon_Set.h
@@ -0,0 +1,21 @@
+#ifndef CYPHER_COMMON_TIER1_SET_H
+#define CYPHER_COMMON_TIER1_SET_H
+#pragma once
+
+/*
+================
+CypherCommon Set
+
+Ordered set declarations.
+================
+*/
+
+namespace cypher::common
+{
+
+template <typename key_t>
+struct set_t;
+
+} // namespace cypher::common
+
+#endif // CYPHER_COMMON_TIER1_SET_H
